681ba7cf82b57a5d2181edd076af568e8c1b50c2
skaki2019 <skakihara@gmail.com>
2022-11-01 22:14:21 +0900

    website 1

--- R/rblas.R
+++ R/rblas.R
@@ -11,6 +11,12 @@ norm2a <- function(x) .Call("norm2_", x)
 nullp1 <- function(x) .Call("nullp_", x)
 
 #' Compute incomplete gamma
+#' 
+#' @description Evaluate an incomplete gamma function:
+#' \deqn{\gamma(a, x) = \int_{0}^{x} t^{a-1}\exp{-t}dt,}
+#' using SLATEC `dgami` in \url{https://netlib.org/slatec/}.
+#' When \eqn{a \gt 0} and \eqn{x \ge 0}, compute the result, otherwise the value
+#' is `NaN`.
 #' @param a A numeric vector.
 #' @param x A numeric vector
 #' @return `dgami(a, x)`
@@ -18,6 +24,12 @@ nullp1 <- function(x) .Call("nullp_", x)
 igamma <- function(a, x) .Call("igamma_", a, x)
 
 #' Compute complementary incomplete gamma
+#' 
+#' @description Evaluate an complementary incomplete gamma function:
+#' \deqn{\gamma^{*}(a, x) = \int_{x}^{\inf} t^{a-1}\exp{-t}dt,}
+#' using SLATEC `dgamic` in \url{https://netlib.org/slatec/}.
+#' When \eqn{x > 0} or \eqn{x \eq 0 & a \ge 0}, compute the result,
+#' otherwise the value is `NaN`.
 #' @param a A numeric vector.
 #' @param x A numeric vector
 #' @return `dgamic(a, x)`
